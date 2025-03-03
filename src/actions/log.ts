02346b91f0998325040c189bbf733ea90fcff123
JB <jbb@jbb.dev>
2025-03-03 13:40:09 -0800

    Initial commit

---
+++ src/actions/log.ts
@@ -0,0 +1,46 @@
+import {
+  Category,
+  FieldType,
+  type Action,
+  type DataPointDefinition,
+  type Field,
+} from '@awell-health/extensions-core'
+import { type settings } from '../settings'
+
+const fields = {
+  hello: {
+    id: 'hello',
+    label: 'Hello',
+    description: 'A string field configured at design time',
+    type: FieldType.STRING,
+  },
+} satisfies Record<string, Field>
+
+const dataPoints = {
+  world: {
+    key: 'world',
+    valueType: 'string',
+  },
+} satisfies Record<string, DataPointDefinition>
+
+export const log: Action<
+  typeof fields,
+  typeof settings,
+  keyof typeof dataPoints
+> = {
+  key: 'log',
+  category: Category.DEMO,
+  title: 'Log hello world',
+  description: 'This is a dummy Custom Action for extension developers.',
+  fields,
+  previewable: true,
+  dataPoints,
+  onEvent: async ({ payload, onComplete }) => {
+    const { fields } = payload
+    await onComplete({
+      data_points: {
+        world: fields.hello,
+      },
+    })
+  },
+}
---
+++ src/settings.ts
@@ -0,0 +1,11 @@
+import { type Setting } from '@awell-health/extensions-core'
+
+export const settings = {
+  secret: {
+    key: 'secret',
+    label: 'Secret',
+    obfuscated: true,
+    description: 'A secret value needed by the extension like an API key.',
+    required: false,
+  },
+} satisfies Record<string, Setting>
